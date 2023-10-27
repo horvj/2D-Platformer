4eebcb68e5bb10f45b769e924d66cbce188336f1
horvj <horvathjack1@gmail.com>
2023-10-27 11:14:02 -0400

    uppdate

--- UI/LevelScene.gd
+++ UI/LevelScene.gd
@@ -2,8 +2,8 @@ extends Control
 
 
 func _on_battlefield_button_down():
-	get_tree().change_scene("res://Levels/battlefield.tscn")
+	get_tree().change_scene_to_file("res://Levels/battlefield.tscn")
 
 
 func _on_plane_button_down():
-	get_tree().change_scene("res://Levels/plane.tscn")
+	get_tree().change_scene_to_file("res://Levels/plane.tscn")
--- UI/Player2Select.gd
+++ UI/Player2Select.gd
@@ -2,15 +2,15 @@ extends Control
 
 func _on_swordsman_button_down():
 	Global.player2Input = "Swordsman"
-	get_tree().change_scene("res://LevelScene.tscn")
-
+	get_tree().change_scene_to_file("res://UI/LevelScene.tscn")
 
 
 func _on_archer_button_down():
 	Global.player2Input = "Archer"
-	get_tree().change_scene("res://LevelScene.tscn")
+	get_tree().change_scene_to_file("res://UI/LevelScene.tscn")
 
 
 func _on_wizard_button_down():
 	Global.player2Input = "Wizard"
-	get_tree().change_scene("res://LevelScene.tscn")
+	get_tree().change_scene_to_file("res://UI/LevelScene.tscn")
+
